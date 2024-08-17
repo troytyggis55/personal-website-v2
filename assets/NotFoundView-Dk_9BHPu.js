1087810d22f4dcdb896e97d076dffd45171e52a1
Trygve Jørgensen <trygjor@stud.ntnu.no>
2024-08-18 01:02:20 +0200

    Updates

--- assets/NotFoundView-Dk_9BHPu.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as o,c as t,b as c}from"./index-BWHCBHlE.js";const n={},s=c("h1",null,"404 - Not Found",-1),_=[s];function a(r,d){return o(),t("div",null,_)}const i=e(n,[["render",a]]);export{i as default};
